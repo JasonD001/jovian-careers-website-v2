4307458ac7438fdabcfb91546dd8905949af432e
RAHUL_DA <143429212+JasonD001@users.noreply.github.com>
2024-07-08 06:31:14 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#   return render_template('home.html')
+
+# print(__name__)
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', debug=True)
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello World</p>"
+  return render_template('home.html')
 
 
-print(__name__)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
